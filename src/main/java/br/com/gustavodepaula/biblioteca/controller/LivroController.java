23d4fea39a525cb50a4996556d672a267b564764
Gustavo de Paula <depaula.gustavo@outlook.com>
2022-10-04 23:56:28 -0300

    Adicionado metodo para listagem dos livros contidos no BD

--- src/main/java/br/com/gustavodepaula/biblioteca/controller/LivroController.java
+++ src/main/java/br/com/gustavodepaula/biblioteca/controller/LivroController.java
@@ -1,7 +1,22 @@
 package br.com.gustavodepaula.biblioteca.controller;
 
+import br.com.gustavodepaula.biblioteca.dto.LivroDto;
+import br.com.gustavodepaula.biblioteca.model.Livro;
+import br.com.gustavodepaula.biblioteca.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LivroController {
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @RequestMapping("/livros")
+    public List<LivroDto> listarLivros(){
+        List<Livro> livros = livroRepository.findAll();
+        return LivroDto.converter(livros);
+    }
 }
