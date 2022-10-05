c6a78312f864d476f9d1e8d5d55ac839d52ceaf0
Gustavo de Paula <depaula.gustavo@outlook.com>
2022-10-04 22:39:40 -0300

    Adicionados os atributos, construtor e getters de LivroDto

--- src/main/java/br/com/gustavodepaula/biblioteca/dto/LivroDto.java
+++ src/main/java/br/com/gustavodepaula/biblioteca/dto/LivroDto.java
@@ -1,4 +1,25 @@
 package br.com.gustavodepaula.biblioteca.dto;
 
+import br.com.gustavodepaula.biblioteca.model.Livro;
+
 public class LivroDto {
+    private Long id;
+    private String nome;
+    private String genero;
+
+    public LivroDto(Livro livro){
+        this.id = livro.getId();
+        this.nome = livro.getNome();
+        this.genero = livro.getGenero().toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getGenero() {
+        return genero;
+    }
 }
