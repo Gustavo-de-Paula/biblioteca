806cba1f2703d4961bd0f55e77c0913016103a1e
Gustavo de Paula <depaula.gustavo@outlook.com>
2022-10-05 20:11:58 -0300

    Adicionada anotação necessaria para paginação dos endpoints

--- src/main/java/br/com/gustavodepaula/biblioteca/BibliotecaApplication.java
+++ src/main/java/br/com/gustavodepaula/biblioteca/BibliotecaApplication.java
@@ -2,8 +2,10 @@ package br.com.gustavodepaula.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class BibliotecaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
