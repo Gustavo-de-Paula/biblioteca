adafd50f821d5ef2ff27044d5c1dbdfefbbca5be
Gustavo de Paula <depaula.gustavo@outlook.com>
2022-10-05 09:05:57 -0300

    Alterados os mappings do controller de livros

--- src/main/java/br/com/gustavodepaula/biblioteca/controller/LivroController.java
+++ src/main/java/br/com/gustavodepaula/biblioteca/controller/LivroController.java
@@ -4,18 +4,21 @@ import br.com.gustavodepaula.biblioteca.dto.LivroDto;
 import br.com.gustavodepaula.biblioteca.model.Livro;
 import br.com.gustavodepaula.biblioteca.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/livros")
 public class LivroController {
     @Autowired
     private LivroRepository livroRepository;
 
-    @RequestMapping("/livros")
-    public List<LivroDto> listarLivros(String autor){
+    @GetMapping
+    public List<LivroDto> listarLivros(String autor) {
         if (autor == null) {
             List<Livro> livros = livroRepository.findAll();
             return LivroDto.converter(livros);
