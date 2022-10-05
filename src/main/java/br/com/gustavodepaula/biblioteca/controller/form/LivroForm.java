00c7a79502fb851f46f914b0861f4602c8daab40
Gustavo de Paula <depaula.gustavo@outlook.com>
2022-10-05 09:17:09 -0300

    Adicionada classe para cadastro de livros

---
+++ src/main/java/br/com/gustavodepaula/biblioteca/controller/form/LivroForm.java
@@ -0,0 +1,26 @@
+package br.com.gustavodepaula.biblioteca.controller.form;
+
+public class LivroForm {
+    private String nome;
+    private String autor;
+    private String genero;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
