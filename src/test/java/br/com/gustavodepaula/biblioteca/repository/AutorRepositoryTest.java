65f7ba986a018273d55758ca4e3aaf82a915b12b
Gustavo de Paula <depaula.gustavo@outlook.com>
2022-10-08 18:36:48 -0300

    Adicionadas configurações para o perfil de testes

--- src/test/java/br/com/gustavodepaula/biblioteca/repository/AutorRepositoryTest.java
+++ src/test/java/br/com/gustavodepaula/biblioteca/repository/AutorRepositoryTest.java
@@ -4,23 +4,34 @@ import br.com.gustavodepaula.biblioteca.model.Autor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class AutorRepositoryTest {
     @Autowired
     private AutorRepository repository;
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
     public void CarregaUmAutorAoBuscarPeloNome() {
         String nomeAutor = "WILLIAM GIBSON";
-        Autor autor =  repository.findByNome(nomeAutor).get();
+        Autor autor =  new Autor();
+        autor.setNome(nomeAutor);
+        entityManager.persist(autor);
+
         Assertions.assertNotNull(autor);
         Assertions.assertEquals(nomeAutor, autor.getNome());
     }
 
     @Test
     public void naoCarregaAutorCasoNomeNaoEstejaNaDatabase() {
-        String nomeAutor = "H P LOVECRAFT";
+        String nomeAutor = "WILLIAM GIBSON";
         Autor autor =  repository.findByNome(nomeAutor).orElse(null);
         Assertions.assertNull(autor);
     }
